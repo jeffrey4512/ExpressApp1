6f00612b7d1b34b78be234b1761c3b20e8488178
jeffrey ng <jeffrey_ng1000@hotmail.com>
2021-10-17 14:47:48 +0800

    minor change

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('./connection');
+var getUserCount = 'SELECT COUNT(*) AS userCount FROM users';
 var getTotalSales = 'SELECT SUM(quantity) AS sales FROM order_items';
 var getMonthlySales = 'SELECT SUM(totalcost) as MonthlyEarnings, sum(Monthlyqty) as MonthlySale FROM('
     + ' SELECT SUM(oi.quantity) as Monthlyqty, SUM(price * oi.quantity) AS totalcost FROM order_items oi'
@@ -11,17 +12,23 @@ var getMonthlySales = 'SELECT SUM(totalcost) as MonthlyEarnings, sum(Monthlyqty)
     + ' GROUP BY oi.product_id) AS T;';
   
 router.get('/', (req, res) => {
-    connection.query(getTotalSales, function (err, result) {
+    connection.query(getTotalSales,  (err, result) => {
         if (err) throw err; 
         if (result.length) {
-            connection.query(getMonthlySales, function (err, subresult) {
+            connection.query(getMonthlySales,  (err, subresult) => {
                 if (err) throw err; 
                 if (result.length) {
-                    res.render('admin', {
-                        earningsM: subresult[0].MonthlyEarnings,
-                        earningsY: '$20000',
-                        sales: result[0].sales
+                    connection.query(getUserCount, (err, subresult2) => {
+                        res.render('admin', {
+                            earningsM: subresult[0].MonthlyEarnings,
+                            earningsY: '$20000',
+                            sales: result[0].sales,
+                            userCount: subresult2[0].userCount
+                        });
                     });
+
+
+
                 }
             }); 
         }
